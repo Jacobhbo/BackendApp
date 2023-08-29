a5246a8b147ff5bc443ccb3cd5baa818bff681bf
Tec <Tec@SKAB1-PC7>
2023-08-29 10:46:10 +0100

    Add project files.

---
+++ BackendApp/Factory/OSFactory.cs
@@ -0,0 +1,27 @@
+﻿using BackendApp.Interface;
+using BackendApp.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendApp.Factory
+{
+    public class OSFactory
+    {
+        public IOS Indentify(string minUserFolder)
+        {
+            if (minUserFolder.Contains("C:"))
+            {
+                return new Windows("Niels Olesen");
+            }
+            else if (minUserFolder.Contains('/'))
+            {
+                return new Linux("Niels Olesen");
+            }
+            else
+                return null;
+        }
+    }
+}
---
+++ BackendApp/Types/Windows.cs
@@ -0,0 +1,21 @@
+﻿using BackendApp.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendApp.Types
+{
+    public class Windows : OS, IOS
+    {
+        public Windows(string userFullName) : base(userFullName)
+        {
+        }
+
+        public string ShowOSType()
+        {
+            return $"Welcome {UserFullName} to windows, running from backend!";
+        }
+    }
+}
